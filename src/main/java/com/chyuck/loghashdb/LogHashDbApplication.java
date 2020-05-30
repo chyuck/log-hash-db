8955a6b43affbe2f4adb9aeac00bbb12277a5e00
asergeev <asergeev@cargurus.com>
2020-05-30 16:26:14 -0400

    Initial Spring Boot files

---
+++ src/main/java/com/chyuck/loghashdb/LogHashDbApplication.java
@@ -0,0 +1,13 @@
+package com.chyuck.loghashdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogHashDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogHashDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chyuck/loghashdb/LogHashDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chyuck.loghashdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHashDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
